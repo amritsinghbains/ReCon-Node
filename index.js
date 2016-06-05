f0562ec853c20c5bc1f084442c5228c79481f0ca
Amrit Bains <bains118@cspc1.uwindsor.ca>
2016-06-04 20:44:20 -0400

    console log2

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ app.post('/ipandwebsite', function (req, res) {
 });
 
 app.post('/websiteandleakiness', function (req, res) {    
+    console.log('Website: ' + req.body.website + ' Leakiness: ' + req.body.leakiness);
     if(req.body.leakiness != undefined && req.body.website != undefined){
       ipandwebsite(req.body.website, req.body.leakiness);
-      console.log('Website: ' + req.body.website + ' Leakiness: ' + req.body.leakiness);
       res.send('Website: ' + req.body.website + ' Leakiness: ' + req.body.leakiness);
     }else {
       res.send('No Support yet'); 
